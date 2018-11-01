144e99ca4080e3a3a3267f0eceed7c46c1cf56e9
Roger Bif <rogerbif@gmail.com>
2018-10-31 22:09:54 -0300

    Ajustes na tela add pet

--- pets/add.php
+++ pets/add.php
@@ -62,6 +62,7 @@ Castrado - castrated -
             <input type="date" class="form-control" name="pet['birthdate']"> 
 		</div>
     </div>
+	
     <div class="row">
         <div class="form-group col-md-5">
             <label for="campo1">Especie</label>
@@ -73,32 +74,43 @@ Castrado - castrated -
             <input type="text" class="form-control" name="pet['Breed']"> 
 		</div>
 		
-        <div class="form-group col-md-2">
-            <label for="campo3">Pelo</label>
-            <input type="text" class="form-control" name="pet['fur']"> 
+		<div class="form-group col-md-2">
+			<label for="campo3">Pelo</label>
+			<select class="form-control" id="campo3" name="pet['fur']">
+			  <option value="Curto">Curto</option>
+			  <option value="Longo">Longo</option>
+			</select>
 		</div>
-		
+				
         <div class="form-group col-md-2">
             <label for="campo3">Data de Cadastro</label>
             <input type="text" class="form-control" name="pet['created']" disabled> 
 		</div>
     </div>
+	
     <div class="row">
         <div class="form-group col-md-3">
             <label for="campo1">Cor</label>
             <input type="text" class="form-control" name="pet['Color']"> 
 		</div>
 		
-        <div class="form-group col-md-2">
-            <label for="campo2">Sexo</label>
-            <input type="text" class="form-control" name="pet['sex']"> 
+		<div class="form-group col-md-2">
+			<label for="campo2">Sexo</label>
+			<select class="form-control" id="campo2" name="pet['sex']">
+			  <option value="Femea">Fêmea</option>
+			  <option value="Macho">Macho</option>
+			</select>
 		</div>
 		
-        <div class="form-group col-md-2">
-            <label for="campo3">Castrado</label>
-            <input type="text" class="form-control" name="pet['castrated']"> 
-		</div>
+		<div class="form-group col-md-2">
+			<label for="campo3">Castrado</label>
+			<select class="form-control" id="campo3" name="pet['castrated']">
+			  <option value="1">Sim</option>
+			  <option value="0">Não</option>
+			</select>
+		 </div>
     </div>
+	
     <div id="actions" class="row">
         <div class="col-md-12">
             <button type="submit" class="btn btn-primary">Salvar</button> <a href="index.php" class="btn btn-default">Cancelar</a> </div>
